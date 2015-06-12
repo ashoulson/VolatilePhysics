63d81e9e8261978a12e4f8542b7406bebd545966
Alexander Shoulson <ashoulson@gmail.com>
2015-06-11 22:40:56 -0400

    Unity MonoBehaviours
    Starting to make MonoBehaviour components for Volatile bodies and
    shapes.

---
+++ Unity/Assets/Scripts/VolatilePhysics/Unity/VolatileDebug.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Volatile;
+
+public static class VolatileDebug 
+{
+  public static void DrawShape(Polygon polygon)
+  {
+    Color current = Gizmos.color;
+
+    Vector2[] vertices = polygon.WorldVertices;
+    Vector2[] normals = polygon.WorldNormals;
+
+    for (int i = 0; i < vertices.Length; i++)
+    {
+      Vector2 u = vertices[i];
+      Vector2 v = vertices[(i + 1) % vertices.Length];
+      Vector2 n = normals[i];
+
+      Vector2 delta = v - u;
+      Vector2 midPoint = u + (delta * 0.5f);
+
+      // Draw edge
+      Gizmos.color = Color.yellow;
+      Gizmos.DrawLine(u, v);
+
+      // Draw normal
+      Gizmos.color = new Color(1.0f, 0.0f, 1.0f);
+      Gizmos.DrawLine(midPoint, midPoint + (n * 0.25f));
+    }
+
+    Gizmos.color = current;
+  }
+}
---
+++ Unity/Assets/Scripts/VolatilePhysics/Unity/VolatileShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Volatile;
+
+public abstract class VolatileShape : MonoBehaviour
+{
+  public abstract Shape Shape { get; }
+
+  public abstract Shape PrepareShape(VolatileBody body);
+  public abstract void DrawShapeInGame();
+  public abstract void DrawShapeInEditor();
+}
