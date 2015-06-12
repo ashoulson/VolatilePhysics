d2927bdbcfdb3a006cd82469bacf660631e41dfc
ashoulson <ashoulson@gmail.com>
2015-06-11 20:30:11 -0400

    Adding initial Volatile files

---
+++ Unity/Assets/Scripts/VolatilePhysics/Volatile/Collision/Collision.cs
@@ -0,0 +1,257 @@
+﻿/*
+ *  VolatilePhysics - A 2D Physics Library for Networked Games
+ *  Copyright (c) 2015 - Alexander Shoulson - http://ashoulson.com
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty. In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *  
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Volatile
+{
+  internal static class Collision
+  {
+    #region Dispatch
+    private delegate bool CollisionTest(Shape sa, Shape sb, ref Manifold m);
+    private readonly static CollisionTest[,] tests = new CollisionTest[,]
+      {
+        { __Circle_Circle, __Circle_Polygon },
+        { __Polygon_Circle, __Polygon_Polygon}
+      };
+
+    internal static bool Dispatch(Shape sa, Shape sb, ref Manifold m)
+    {
+      CollisionTest test = Collision.tests[(int)sa.Type, (int)sb.Type];
+      return test(sa, sb, ref m);
+    }
+
+    internal static bool __Circle_Circle(Shape sa, Shape sb, ref Manifold m)
+    {
+      return Circle_Circle((Circle)sa, (Circle)sb, ref m);
+    }
+
+    internal static bool __Circle_Polygon(Shape sa, Shape sb, ref Manifold m)
+    {
+      return Circle_Polygon((Circle)sa, (Polygon)sb, ref m);
+    }
+
+    internal static bool __Polygon_Circle(Shape sa, Shape sb, ref Manifold m)
+    {
+      return Circle_Polygon((Circle)sb, (Polygon)sa, ref m);
+    }
+
+    internal static bool __Polygon_Polygon(Shape sa, Shape sb, ref Manifold m)
+    {
+      return Polygon_Polygon((Polygon)sa, (Polygon)sb, ref m);
+    }
+    #endregion
+
+    #region Collision Tests
+    internal static bool Circle_Circle(
+      Circle circA,
+      Circle circB,
+      ref Manifold manifold)
+    {
+      return TestCircles(
+        circA.cachedWorldCenter,
+        circB.cachedWorldCenter,
+        circA.Radius,
+        circB.Radius,
+        ref manifold);
+    }
+
+    internal static bool Circle_Polygon(
+      Circle circ,
+      Polygon poly,
+      ref Manifold manifold)
+    {
+      // Get the axis on the polygon closest to the circle's origin
+      float penetration;
+      int ix = FindNearestAxis(circ, poly, out penetration);
+      if (ix < 0)
+        return false;
+      Vector2 v =
+        poly.cachedWorldVertices[ix];
+      Vector2 u =
+        poly.cachedWorldVertices[(ix + 1) % poly.cachedWorldAxes.Length];
+      Axis a = poly.cachedWorldAxes[ix];
+
+      // If the circle is past one of the two vertices, check it like
+      // a circle-circle intersection where the vertex has radius 0
+      float d = Util.Cross(a.Normal, circ.cachedWorldCenter);
+      if (d > Util.Cross(a.Normal, v))
+        return Collision.TestCircles(
+          circ.cachedWorldCenter, v, circ.Radius, 0, ref manifold);
+      if (d < Util.Cross(a.Normal, u))
+        return Collision.TestCircles(
+          circ.cachedWorldCenter, u, circ.Radius, 0, ref manifold);
+
+      // Create/Update the Manifold
+      if (manifold == null)
+        manifold = new Manifold(1);
+      Vector2 pos =
+        circ.cachedWorldCenter - (circ.Radius + penetration / 2) * a.Normal;
+      manifold.UpdateContact(pos, -a.Normal, penetration, 0);
+      return true;
+    }
+
+    internal static bool Polygon_Polygon(
+      Polygon poly1,
+      Polygon poly2,
+      ref Manifold manifold)
+    {
+      Axis a1, a2;
+      if (Collision.FindMinSepAxis(poly1, poly2, out a1) == false)
+        return false;
+      if (Collision.FindMinSepAxis(poly2, poly1, out a2) == false)
+        return false;
+
+      // We will use poly1's axis, so we may need to swap
+      if (a2.Width > a1.Width)
+      {
+        Util.Swap(ref poly1, ref poly2);
+        Util.Swap(ref a1, ref a2);
+      }
+
+      // Create/Update the Manifold (note we may have swapped shapes)
+      if (manifold == null)
+        manifold = new Manifold(3);
+      manifold.shapeA = poly1;
+      manifold.shapeB = poly2;
+      Collision.FindVerts(poly1, poly2, a1.Normal, a1.Width, manifold);
+      return true;
+    }
+    #endregion
+
+    #region Internals
+    /// <summary>
+    /// Returns the index of the nearest axis on the poly to the circle.
+    /// </summary>
+    private static int FindNearestAxis(
+      Circle circ,
+      Polygon poly,
+      out float penetration)
+    {
+      int ix = 0;
+      penetration = float.NegativeInfinity;
+
+      for (int i = 0; i < poly.cachedWorldAxes.Length; i++)
+      {
+        float dot =
+          Vector2.Dot(
+            poly.cachedWorldAxes[i].Normal,
+            circ.cachedWorldCenter);
+        float dist = dot - poly.cachedWorldAxes[i].Width - circ.Radius;
+
+        if (dist > 0)
+          return -1;
+        if (dist > penetration)
+        {
+          penetration = dist;
+          ix = i;
+        }
+      }
+      return ix;
+    }
+
+    /// <summary>
+    /// Workhorse for circle-circle collisions, compares origin distance
+    /// to the sum of the two circles' radii (and writes to the Manifold).
+    /// </summary>
+    private static bool TestCircles(
+      Vector2 circ1,
+      Vector2 circ2,
+      float radius1,
+      float radius2,
+      ref Manifold manifold)
+    {
+      Vector2 r = circ2 - circ1;
+      float min = radius1 + radius2;
+      float distSq = r.sqrMagnitude;
+
+      if (distSq >= min * min)
+        return false;
+
+      float dist = Mathf.Sqrt(distSq);
+      float distInv = 1.0f / dist;
+
+      // Create/Update the Manifold
+      if (manifold == null)
+        manifold = new Manifold(1);
+      Vector2 pos = circ1 + (0.5f + distInv * (radius1 - min / 2.0f)) * r;
+      manifold.UpdateContact(pos, distInv * r, dist - min, 0);
+      return true;
+    }
+
+    private static bool FindMinSepAxis(
+      Polygon poly1,
+      Polygon poly2,
+      out Axis axis)
+    {
+      axis = new Axis(Vector2.zero, float.NegativeInfinity);
+      foreach (Axis a in poly1.cachedWorldAxes)
+      {
+        float min = float.PositiveInfinity;
+        foreach (Vector2 v in poly2.cachedWorldVertices)
+          min = Mathf.Min(min, Vector2.Dot(a.Normal, v));
+        min -= a.Width;
+
+        if (min > 0)
+          return false;
+        if (min > axis.Width)
+          axis = new Axis(a.Normal, min);
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// Add contacts for penetrating vertices. Note that this does not handle
+    /// cases where an overlap was detected, but no vertices fall inside the
+    /// opposing polygon (like a Star of David). See Chipmunk's cpCollision.c
+    /// for more details on how this is resolved.
+    /// </summary>
+    private static void FindVerts(
+      Polygon poly1,
+      Polygon poly2,
+      Vector2 normal,
+      float penetration,
+      Manifold manifold)
+    {
+      uint id = poly1.id << 8;
+      foreach (Vector2 v in poly1.cachedWorldVertices)
+      {
+        if (poly2.ContainsVert(v))
+          if (manifold.UpdateContact(v, normal, penetration, id) == false)
+            return;
+        id++;
+      }
+
+      id = poly2.id << 8;
+      foreach (Vector2 v in poly2.cachedWorldVertices)
+      {
+        if (poly1.ContainsVert(v))
+          if (manifold.UpdateContact(v, normal, penetration, id) == false)
+            return;
+        id++;
+      }
+    }
+    #endregion
+  }
+}
---
+++ Unity/Assets/Scripts/VolatilePhysics/Volatile/Collision/Contact.cs
@@ -0,0 +1,176 @@
+﻿/*
+ *  VolatilePhysics - A 2D Physics Library for Networked Games
+ *  Copyright (c) 2015 - Alexander Shoulson - http://ashoulson.com
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty. In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *  
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Volatile
+{
+  public sealed class Contact
+  {
+    #region Static Methods
+    private static float BiasDist(float dist)
+    {
+      return Config.RESOLVE_RATE * Mathf.Min(0, dist + Config.RESOLVE_SLOP);
+    }
+    #endregion
+
+    public Vector2 position;
+    public float penetration;
+    public float cachedNormalImpulse;
+    public float cachedTangentImpulse;
+
+    public Vector2 normal;
+    private Vector2 toA;
+    private Vector2 toB;
+
+    private float nMass;
+    private float tMass;
+    private float restitution;
+    private float bias;
+    private float jBias;
+
+    internal uint id;
+    internal bool updated;
+
+    public Contact()
+    {
+    }
+
+    internal void Prestep(Manifold manifold)
+    {
+      Body bodyA = manifold.shapeA.body;
+      Body bodyB = manifold.shapeB.body;
+
+      this.toA = this.position - bodyA.Position;
+      this.toB = this.position - bodyB.Position;
+
+      this.nMass = 1.0f / this.KScalar(bodyA, bodyB, this.normal);
+      this.tMass = 1.0f / this.KScalar(bodyA, bodyB, this.normal.Left());
+
+      this.bias = Contact.BiasDist(penetration);
+      this.jBias = 0;
+      this.restitution =
+        manifold.restitution *
+        Vector2.Dot(
+          this.normal,
+          this.RelativeVelocity(bodyA, bodyB));
+    }
+
+    internal void SolveCached(Manifold manifold)
+    {
+      this.ApplyContactImpulse(
+        manifold.shapeA.body,
+        manifold.shapeB.body,
+        this.cachedNormalImpulse,
+        this.cachedTangentImpulse);
+    }
+
+    internal void Solve(Manifold manifold)
+    {
+      Body bodyA = manifold.shapeA.body;
+      Body bodyB = manifold.shapeB.body;
+
+      // Calculate relative bias velocity
+      Vector2 vb1 =
+        bodyA.velBias + (bodyA.rotBias * this.toA.Left());
+      Vector2 vb2 =
+        bodyB.velBias + (bodyB.rotBias * this.toB.Left());
+      float vbn = Vector2.Dot((vb1 - vb2), this.normal);
+
+      // Calculate and clamp the bias impulse
+      float jbn = this.nMass * (vbn - this.bias);
+      jbn = Mathf.Max(-this.jBias, jbn);
+      this.jBias += jbn;
+
+      // Apply the bias impulse
+      this.ApplyNormalBiasImpulse(bodyA, bodyB, jbn);
+
+      // Calculate relative velocity
+      Vector2 vr = this.RelativeVelocity(bodyA, bodyB);
+      float vrn = Vector2.Dot(vr, this.normal);
+
+      // Calculate and clamp the normal impulse
+      float jn = nMass * (vrn + (this.restitution * World.elasticity));
+      jn = Mathf.Max(-this.cachedNormalImpulse, jn);
+      this.cachedNormalImpulse += jn;
+
+      // Calculate the relative tangent velocity
+      float vrt = Vector2.Dot(vr, this.normal.Left());
+
+      // Calculate and clamp the friction impulse
+      float jtMax = manifold.friction * this.cachedNormalImpulse;
+      float jt = vrt * tMass;
+      float result =
+        Mathf.Clamp(this.cachedTangentImpulse + jt, -jtMax, jtMax);
+      jt = result - this.cachedTangentImpulse;
+      this.cachedTangentImpulse = result;
+
+      // Apply the normal and tangent impulse
+      this.ApplyContactImpulse(bodyA, bodyB, jn, jt);
+    }
+
+    #region Internals
+    private float KScalar(
+      Body bodyA,
+      Body bodyB,
+      Vector2 normal)
+    {
+      float massSum = bodyA.massInv + bodyB.massInv;
+      float r1cnSqr = Util.Square(Util.Cross(this.toA, normal));
+      float r2cnSqr = Util.Square(Util.Cross(this.toB, normal));
+      return massSum + bodyA.inertiaInv * r1cnSqr + bodyB.inertiaInv * r2cnSqr;
+    }
+
+    private Vector2 RelativeVelocity(Body bodyA, Body bodyB)
+    {
+      return
+        (bodyA.AngularVelocity * this.toA.Left() + bodyA.LinearVelocity) -
+        (bodyB.AngularVelocity * this.toB.Left() + bodyB.LinearVelocity);
+    }
+
+    private void ApplyNormalBiasImpulse(
+      Body bodyA,
+      Body bodyB,
+      float normalBiasImpulse)
+    {
+      Vector2 impulse = normalBiasImpulse * this.normal;
+      bodyA.ApplyBias(-impulse, this.toA);
+      bodyB.ApplyBias(impulse, this.toB);
+    }
+
+    private void ApplyContactImpulse(
+      Body bodyA,
+      Body bodyB,
+      float normalImpulseMagnitude,
+      float tangentImpulseMagnitude)
+    {
+      Vector2 impulseWorld =
+        new Vector2(normalImpulseMagnitude, tangentImpulseMagnitude);
+      Vector2 impulse = impulseWorld.Rotate(this.normal);
+
+      bodyA.ApplyImpulse(-impulse, this.toA);
+      bodyB.ApplyImpulse(impulse, this.toB);
+    }
+    #endregion
+  }
+}
