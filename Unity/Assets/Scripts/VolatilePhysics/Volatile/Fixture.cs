e5eb810c632fa9af3eb301fcb4a9d9c80ec5d0f2
Alexander Shoulson <ashoulson@gmail.com>
2015-06-21 00:16:06 -0400

    Cleanup in Polygon and Fixture

--- Unity/Assets/Scripts/VolatilePhysics/Volatile/Fixture.cs
+++ Unity/Assets/Scripts/VolatilePhysics/Volatile/Fixture.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ *  VolatilePhysics - A 2D Physics Library for Networked Games
+ *  Copyright (c) 2015 - Alexander Shoulson - http://ashoulson.com
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty. In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *  
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -38,6 +58,10 @@ namespace Volatile
       this.facingOffset = ComputeFacingOffset(body, shape);
     }
 
+    /// <summary>
+    /// Converts the body's position and facing into a world space 
+    /// position and facing for the shape, and sets it on the shape.
+    /// </summary>
     internal void Apply(Vector2 bodyPosition, Vector2 bodyFacing)
     {
       Vector2 shapePosition =
