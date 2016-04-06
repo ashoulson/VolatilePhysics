b09e80df8fa0f2c6ea24c9a8e814c7f89f529387
ashoulson <ashoulson@gmail.com>
2016-04-05 23:28:33 -0400

    Testing migration of some changes

--- Unity/Assets/Scripts/VolatilePhysics/Unity/VolatileShape.cs
+++ Unity/Assets/Scripts/VolatilePhysics/Unity/VolatileShape.cs
@@ -10,14 +10,8 @@ public abstract class VolatileShape : MonoBehaviour
   [SerializeField]
   protected float density = 1.0f;
 
-  public abstract Shape Shape { get; }
-  public abstract void PrepareShape();
+  public abstract VoltShape PrepareShape(VoltWorld world);
 
   public abstract void DrawShapeInEditor();
   public abstract Vector2 ComputeTrueCenterOfMass();
-
-  void Awake()
-  {
-    this.PrepareShape();
-  }
 }
